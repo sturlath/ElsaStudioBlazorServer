39de6fe8a45c4cb62822c66afc17beebe50f79de
Sturla Þorvaldsson <sturla.thorvaldsson@marel.com>
2024-08-09 10:46:22 +0000

    Server added

--- Program.cs
+++ Program.cs
@@ -1,11 +1,14 @@
+using Elsa.EntityFrameworkCore.Modules.Management;
+using Elsa.EntityFrameworkCore.Modules.Runtime;
+using Elsa.Extensions;
 using Elsa.Studio.Core.BlazorServer.Extensions;
 using Elsa.Studio.Dashboard.Extensions;
 using Elsa.Studio.Extensions;
 using Elsa.Studio.Login.BlazorServer.Extensions;
 using Elsa.Studio.Login.HttpMessageHandlers;
 using Elsa.Studio.Shell.Extensions;
-using Elsa.Studio.Workflows.Extensions;
 using Elsa.Studio.Workflows.Designer.Extensions;
+using Elsa.Studio.Workflows.Extensions;
 
 // Build the host.
 var builder = WebApplication.CreateBuilder(args);
@@ -36,6 +39,58 @@ builder.Services.AddSignalR(options =>
     options.MaximumReceiveMessageSize = 5 * 1024 * 1000; // 5MB
 });
 
+// Add Elsa services.
+builder.Services.AddElsa(elsa =>
+{
+    // Configure Management layer to use EF Core.
+    elsa.UseWorkflowManagement(management => management.UseEntityFrameworkCore());
+
+    // Configure Runtime layer to use EF Core.
+    elsa.UseWorkflowRuntime(runtime => runtime.UseEntityFrameworkCore());
+
+    // Default Identity features for authentication/authorization.
+    elsa.UseIdentity(identity =>
+    {
+        identity.TokenOptions = options => options.SigningKey = "sufficiently-large-secret-signing-key"; // This key needs to be at least 256 bits long.
+        identity.UseAdminUserProvider();
+    });
+
+    // Configure ASP.NET authentication/authorization.
+    elsa.UseDefaultAuthentication(auth => auth.UseAdminApiKey());
+
+    // Expose Elsa API endpoints.
+    elsa.UseWorkflowsApi();
+
+    // Setup a SignalR hub for real-time updates from the server.
+    elsa.UseRealTimeWorkflows();
+
+    // Enable C# workflow expressions
+    elsa.UseCSharp();
+
+    // Enable HTTP activities.
+    elsa.UseHttp();
+
+    // Use timer activities.
+    elsa.UseScheduling();
+
+    // Register custom activities from the application, if any.
+    elsa.AddActivitiesFrom<Program>();
+
+    // Register custom workflows from the application, if any.
+    elsa.AddWorkflowsFrom<Program>();
+});
+
+// Configure CORS to allow designer app hosted on a different origin to invoke the APIs.
+builder.Services.AddCors(cors => cors
+    .AddDefaultPolicy(policy => policy
+        .AllowAnyOrigin() // For demo purposes only. Use a specific origin instead.
+        .AllowAnyHeader()
+        .AllowAnyMethod()
+        .WithExposedHeaders("x-elsa-workflow-instance-id"))); // Required for Elsa Studio in order to support running workflows from the designer. Alternatively, you can use the `*` wildcard to expose all headers.
+
+// Add Health Checks.
+builder.Services.AddHealthChecks();
+
 // Build the application.
 var app = builder.Build();
 
@@ -51,9 +106,19 @@ if (!app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 app.UseStaticFiles();
 app.UseRouting();
+app.UseAuthentication();
 app.UseAuthorization();
 app.MapBlazorHub();
 app.MapFallbackToPage("/_Host");
+
+// Configure web application's middleware pipeline.
+app.UseCors();
+app.UseRouting(); // Required for SignalR.
+app.UseAuthentication();
+app.UseAuthorization();
+app.UseWorkflowsApi(); // Use Elsa API endpoints.
+app.UseWorkflows(); // Use Elsa middleware to handle HTTP requests mapped to HTTP Endpoint activities.
+app.UseWorkflowsSignalRHubs(); // Optional SignalR integration. Elsa Studio uses SignalR to receive real-time updates from the server. 
 
 // Run the application.
 app.Run();
